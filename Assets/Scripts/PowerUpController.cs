415da5afe2fddef9e30b3a2c8958903ce905e647
chuyalvarez <jesusaagomez@hotmail.com>
2019-03-12 09:41:06 -0600

    powerups now generate randomly and motion blur effect for speed differentiation

---
+++ Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    public GameObject booster;
+    public GameObject slower;
+    private float roll;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 35;i++){
+            roll=Random.Range(0,6);
+            distance=Random.Range(-50,50);
+            if (roll==0){
+                Instantiate(booster,new Vector3((250.0f*i)+200.0f-distance,6.5f,10.0f),Quaternion.identity);
+            } else if (roll==1){
+                Instantiate(slower,new Vector3((250.0f*i)+200.0f-distance,11f,-12.0f),Quaternion.identity);
+            } else if (roll==2){
+                Instantiate(booster,new Vector3((250.0f*i)+200.0f-distance,6.5f,-10.0f),Quaternion.identity);
+            } else if (roll==3){
+                Instantiate(slower,new Vector3((250.0f*i)+200.0f-distance,11f,12.0f),Quaternion.identity);
+            }else if (roll==4){
+                Instantiate(booster,new Vector3((250.0f*i)+200.0f-distance,6.5f,10.0f),Quaternion.identity);
+            } else if (roll==5){
+                Instantiate(booster,new Vector3((250.0f*i)+200.0f-distance,6.5f,-10.0f),Quaternion.identity);
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class carController : MonoBehaviour
 {
     private static float REGULAR_SPEED = 75.0f;
-    private static float FAST_SPEED = 100.0f;
+    private static float FAST_SPEED = 130.0f;
     private static float SLOW_SPEED = 50.0f;
     private float speed = REGULAR_SPEED;
     private float sidemov = 0.0f;
     public float gravity = 20.0f;
     private bool left = true;
+    private int goodbuff=0;
+    private int badbuff=0;
+    private bool coroutinerunning = false;
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
+    private IEnumerator fast;
+    private IEnumerator slow;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        
     }
 
     void Update()
-    {
+    {   
+        Debug.Log(Time.time);
+        if(!coroutinerunning){
+            if (goodbuff >0){
+                goodbuff--;
+                fast=ChangeSpeed(FAST_SPEED);
+                StartCoroutine(fast);
+            } else if(badbuff >0){
+                badbuff--;
+                slow=ChangeSpeed(SLOW_SPEED);
+                StartCoroutine(slow);  
+            } else if(badbuff<1 && goodbuff<1){
+                speed=REGULAR_SPEED;
+            }
+        }
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(1.0f, 0.0f, 1.0f);
@@ -46,24 +66,35 @@ public class carController : MonoBehaviour
 
         moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
+        
     }
 
     IEnumerator ChangeSpeed(float targetSpeed){
+        coroutinerunning=true;
         speed = targetSpeed;
-        yield return new WaitForSeconds(4.0f);
-        speed = REGULAR_SPEED;
+        yield return new WaitForSeconds(2.0f);
+        coroutinerunning=false;
     }
 
     void OnTriggerEnter(Collider c)
     {
 		if(c.gameObject.CompareTag("booster")){
-           
-           StopCoroutine(ChangeSpeed(FAST_SPEED));
-           StartCoroutine(ChangeSpeed(FAST_SPEED));
+            badbuff = 0;
+            goodbuff +=1;
+            if (coroutinerunning && speed<REGULAR_SPEED){
+                StopCoroutine(slow);
+                coroutinerunning=false;
+            }
+            
            
 		}else if(c.gameObject.CompareTag("slower")){
-            StopCoroutine(ChangeSpeed(SLOW_SPEED));
-           StartCoroutine(ChangeSpeed(SLOW_SPEED));
+            goodbuff = 0;
+            badbuff += 1;
+            if (coroutinerunning && speed>REGULAR_SPEED){
+                StopCoroutine(fast);
+                coroutinerunning=false;
+            }
+           
         }
         Destroy(c.gameObject);
     }
